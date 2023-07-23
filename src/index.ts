905696ec8a8da97b0cdd426e7ae4636efe4696d6
mktoho <mk@mktoho.org>
2023-07-23 20:55:51 +0900

    ts3.8 node18

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,46 @@
-import { readFileSync } from 'fs'
-import { findLoop } from './find-it'
+import * as fs from 'fs'
+
+type Node = number
+
+type Input = Node[]
+
+interface Footprints {
+  [key: number]: boolean
+}
+
+// 探索
+const find = (nodes: Input, from = 1, footprints: Footprints = {}): Input => {
+  const next = nodes[from - 1]
+  if (footprints[next]) {
+    return [next]
+  }
+
+  return [next, ...find(nodes, next, { ...footprints, [next]: true })]
+}
+
+const findLoop = (nodes: number[]) => {
+  const loop = find(nodes)
+  const last = loop[loop.length - 1]
+
+  if (last === 0) {
+    // 先頭に戻ってループ
+    return loop
+  } else {
+    // 途中のどこかに戻ってループ
+    const loopStart = loop.findIndex(n => n === last)
+    return loop.slice(loopStart, -1)
+  }
+}
 
 const parse = (inputs: string) =>
   inputs
-    .split(/\n/)?.[1]
+    .split(/\n/)[1]
     .split(/\s+/g)
     .map(n => parseInt(n))
 
 const format = (nums: number[]) => `${nums.length}\n${nums.join(' ')}`
 
-const input = readFileSync('/dev/stdin', 'utf8')
+const input = fs.readFileSync('/dev/stdin', 'utf8')
 const nodes = parse(input)
 const answer = findLoop(nodes)
 const output = format(answer)
